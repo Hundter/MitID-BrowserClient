2c8cda26a41b887e82d610da16b7bf7e9c620376
helmstedt <helstegt@gmail.com>
2024-11-16 15:07:39 +0100

    Also added post.borger.dk

--- login_flows/postborgerdk.py
+++ login_flows/postborgerdk.py
@@ -1,9 +1,9 @@
-# Script for https://www.tastselv.skat.dk
+# Script for https://post.borger.dk
 import requests, json, base64, sys
 from bs4 import BeautifulSoup
 sys.path.append("..")
-from BrowserClient.BrowserClient import BrowserClient
-from BrowserClient.Helpers import get_authentication_code, process_args, get_default_args
+from BrowserClient.Helpers import get_authentication_code, process_args, get_default_args, choose_between_multiple_identitites
+from ScrapingHelp.QueueIt import bypass_botdetect
 
 argparser = get_default_args()
 args = argparser.parse_args()
@@ -13,29 +13,19 @@ session = requests.Session()
 if proxy:
     session.proxies.update({"http": f"socks5://{proxy}", "https": f"socks5://{proxy}" })
 
-# Possibly you are going to need a QueueIT part of the login url.
-# Log on to skat.dk in your browser with developer tools enabled
-# and find the part to add to the url. 
-queue_it_url_params = ""
-
-request = session.get(f"https://www.tastselv.skat.dk/borger/loginsso{queue_it_url_params}")
+# First part of post.borger.dk procedure
+request = bypass_botdetect(session, "https://auth.post.borger.dk/web/auth/login?returnurl=https://post.borger.dk/&idp=nemloginRealm")
 
 if request.status_code != 200:
     print(f"Failed session setup attempt, status code {request.status_code}")
     raise Exception(request.content)
-elif 'vent.skat.dk/softblock' in request.url:
-    print('Queue-it CAPTCHA challenge detected. Trying to pass...')
-    from ScrapingHelp.QueueIt import bypass_botdetect
-    request = bypass_botdetect(session, f"https://www.tastselv.skat.dk/borger/loginsso{queue_it_url_params}")    
-if 'https://vent.skat.dk/?c=skat' in request.url:
-    sys.exit('It seems you have been placed in a Queue-it waiting line. Currently no way to bypass this. Exiting.')
-if request.url != "https://nemlog-in.mitid.dk/login/mitid":
+elif request.url != "https://nemlog-in.mitid.dk/login/mitid":
     print(f"Unexpected URL, maybe something from QueueIT {request.url}")
-    raise Exception(request.content)   
+    raise Exception(request.content)
 
 soup = BeautifulSoup(request.text, 'lxml')
-request_verification_token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
 
+request_verification_token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
 search_string = '"Aux":"'
 start_aux = request.text.index(search_string)+len(search_string)
 end_aux = request.text.index('"', start_aux)
@@ -45,15 +35,20 @@ aux = json.loads(base64.b64decode(request.text[start_aux:end_aux]))
 authorization_code = get_authentication_code(session, aux, method, user_id, password)
 print(f"Your MitID authorization code was ({authorization_code})")
 
-# Second part of skat.dk procedure
+# Second part of mit.dk procedure
 params = {
     "__RequestVerificationToken": request_verification_token,
     "MitIDAuthCode": authorization_code
 }
 
 request = session.post("https://nemlog-in.mitid.dk/login/mitid", data=params)
-
 soup = BeautifulSoup(request.text, features="html.parser")
+
+# User has more than one login option
+if request.url == 'https://nemlog-in.mitid.dk/loginoption':
+    request = choose_between_multiple_identitites(session, request, soup)
+    soup = BeautifulSoup(request.text, "xml")
+        
 relay_state = soup.find('input', {'name': 'RelayState'}).get('value')
 saml_response = soup.find('input', {'name': 'SAMLResponse'}).get('value')
 
@@ -61,10 +56,10 @@ params = {
     "RelayState": relay_state,
     "SAMLResponse": saml_response
 }
-
 request = session.post(soup.form['action'], data=params)
-params = {"post": "ja"}
-request = session.post(request.url, data=params)
-request = session.post(request.url, data=params)
-soup = BeautifulSoup(request.text, 'lxml')
-print(soup.select_one("div.infocontent").text.replace("Log af","").strip())
+
+# Use https://api.post.borger.dk/api/streamaddresses (or developer tools in your browser)
+# for a list of API calls
+session.headers['X-XSRF-TOKEN'] = session.cookies['XSRF-REQUEST-TOKEN']
+request = session.get("https://api.post.borger.dk/api/mailboxes?size=1000")
+print(request.json())
